34035db00dc1519b53183407d53318af6a3df763
amalali8 <amalza958@gmail.com>
2023-08-16 23:33:33 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+function getTotalPrice(groceries) {
+    let totalPrice = 0;
+      for (let i = 0; i < groceries.length; i++) {
+      const { quantity, price } = groceries[i];
+      totalPrice += quantity * price;
+    }
+      return totalPrice;
+  }
+  
+  let groceries = [
+    { product: "Milk", quantity: 9, price: 34 },
+    { product: "Cereals", quantity: 1, price: 2.50 }
+  ];
+  
+  let totalPrice = getTotalPrice(groceries);
+  console.log(totalPrice); 
+  
